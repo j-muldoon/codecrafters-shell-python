07185a0f7a7d01e789266558f0adc5ca82d950a7
j-muldoon <97886015+j-muldoon@users.noreply.github.com>
2025-03-04 16:08:58 +0000

    while loop

--- app/main.py
+++ app/main.py
@@ -2,13 +2,15 @@ import sys
 
 
 def main():
-    # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
 
-    # Wait for user input
-    command = input()
+    while True:
+        # Uncomment this block to pass the first stage
+        sys.stdout.write("$ ")
 
-    print(f"{command}: command not found")
+        # Wait for user input
+        command = input()
+
+        print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
