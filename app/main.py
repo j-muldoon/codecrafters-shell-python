100a3c0eb411f965a691090b4114d168b9d1a827
j-muldoon <97886015+j-muldoon@users.noreply.github.com>
2025-03-04 19:02:47 +0000

    copied, now dependent on just sys and os

--- app/main.py
+++ app/main.py
@@ -1,62 +1,76 @@
 import sys
-import shutil
+import os
 
-def path_search(check_item, path_input):
-    
-    # Going to brute force it, but better way would be to use hash table to not go into directories that have already been covered
+def find_in_path(param):
 
-    directories = path_input.split(":")
+    path = os.environ['PATH']
 
-    for directory in directories:
-        layers = directory.split("/")
-        path_as_list = []
-        for layer in layers:
-            path_as_list.append(layer)
-            if check_item == layer:
-                return True, path_as_list
-    return False, None
-        
+    print("Path: " + path)
+
+    print(f"Param: {param}")
+
+    for directory in path.split(":"):
+
+        for (dirpath, dirnames, filenames) in os.walk(directory):
+
+            if param in filenames:
+
+                return f"{dirpath}/{param}"
+
+    return None
 
 def main():
 
     while True:
-        # Uncomment this block to pass the first stage
+
         sys.stdout.write("$ ")
 
+        sys.stdout.flush()
+
         # Wait for user input
-        user_input = input()
-        words = user_input.split(" ")
-        cmd = words[0]
-        cmd_tail = " ".join(words[1:])
 
-        builtins = ["exit", "echo", "type"]
+        command = input()
 
-        try:
-            
-            if cmd not in builtins:
-                sys.stdout.write(f"{user_input}: command not found\n")
-            elif cmd == "exit":
-                sys.exit(int(cmd_tail))
-            elif cmd == "echo":
-                sys.stdout.write(f"{cmd_tail}\n")
-            elif cmd == "type":
-                
-                if cmd_tail in builtins:
-                    sys.stdout.write(f"{cmd_tail} is a shell builtin\n")
-                elif path := shutil.which(cmd_tail):
-                    print(f"{cmd_tail} is {path}")
+        match command.split(" "):
+
+            case ["exit", "0"]:
+
+                exit(0)
+
+            case ["echo", *cmd]:
+
+                print(" ".join(cmd))
+
+            case ["type", *cmd]:
+
+                match cmd:
+
+                    case ["echo" | "exit" | "type"]:
+
+                        print(f"${cmd[0]} is a shell builtin")
+
+                    case _:
+
+                        location = find_in_path(cmd[0])
+
+                        if location:
+
+                            print(f"${cmd[0]} is {location}")
+
+                        else:
+
+                            print(f"${" ".join(cmd)} not found")
+
+            case _:
+
+                if os.path.isfile(command.split(" ")[0]):
+
+                    os.system(command)
+
                 else:
-                        sys.stdout.write(f"{cmd_tail}: not found\n")
 
-
-        except OSError as err:
-            print("OS error:", err)
-        except ValueError:
-            print("Could not convert data to an integer.")
-        except Exception as err:
-            print(f"Unexpected {err=}, {type(err)=}")
-            raise
-
+                    print(f"{command}: command not found")
 
 if __name__ == "__main__":
+
     main()
